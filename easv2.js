725defd950076ed3dabefd34ced7319df28b8c65
Sean <me@ininger.me>
2019-04-20 15:24:29 -0700

    SYSTEMS NOMINAL

--- easv2.js
+++ easv2.js
@@ -1,17 +1,39 @@
-
-
 //Create a body
 const bodyElement = document.createElement("body");
 document.body = bodyElement;
 const budy = document.querySelector('body');
+
+//create reset button
+const newGridButton = document.createElement('button');
+newGridButton.textContent = "Reset Grid";
+budy.appendChild(newGridButton);
+const resetButton = document.querySelector('button');
 
 //make sure percentage heights scale correctly by assigning height to HTML
 const markup = document.querySelector('html');
 markup.style.height = "100%";
 budy.style.height = "100%";
 
+const resetGrid = () => {
+let rows = document.getElementsByClassName('row');
+//Remove rows, leave button intact
+while(rows[0]) {rows[0].parentNode.removeChild(rows[0])}
+
+let response = prompt("What size grid would you like?");
+let gridSize = parseInt(response);
+createGrid(gridSize);
+}
 
 const createGrid = gridSize => {
+  try {
+    if(gridSize > 64) throw "Grid size should be no larger than 64";
+    if(gridSize <= 0) throw "Grid size should be larger than zero";
+    if(typeof gridSize != "number") throw "Grid size should be passed a whole number between 1-64 as arguments";
+  }
+  catch(err) {
+    budy.innerHTML = "ERROR: " + err;
+    return;
+  }
   //instantiate one row at a time
   for(x=0; x < gridSize; x++) {
     let row = document.createElement("div");
@@ -29,6 +51,19 @@ const createGrid = gridSize => {
       row.appendChild(item);
     }
   }
+  const squares = document.querySelectorAll('div.square');
+  squares.forEach((square)=>{
+    square.addEventListener('mouseover', (e) => {
+       let hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+      square.style.backgroundColor = hue;
+    });
+  });
 }
-
 createGrid(10);
+
+
+
+
+resetButton.addEventListener("click", (e) => {
+  resetGrid();
+});
